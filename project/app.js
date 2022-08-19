73f7d094e7c1a66350f63d402a0de77ab42f0ef3
nakta <nakta0511@gmail.com>
2022-08-19 13:30:22 +0900

    greeting event & saving username

--- project/app.js
+++ project/app.js
@@ -1,9 +1,34 @@
 const loginForm = document.querySelector("#login-form");
 const loginInput = loginForm.querySelector("input");
-const loginButton = loginForm.querySelector("button");
+const greeting = document.querySelector("#greeting");
 
-function onLoginBtnClick() {
-    const value = loginInput.value;
+const HIDDEN_CLASSNAME = "hidden";
+const USERNAME_KEY = "username"
+
+// Event Listener Function
+function onLoginSubmit(event) {
+    const username = loginInput.value;
+
+    // block browser basic function
+    event.preventDefault();
+    loginForm.classList.add(HIDDEN_CLASSNAME);
+    localStorage.setItem(USERNAME_KEY, username);
+    showGreeting(username);
 }
 
-loginButton.addEventListener("click", onLoginBtnClick);
+// Other Function
+function showGreeting(username) {
+    greeting.classList.remove(HIDDEN_CLASSNAME);
+    greeting.innerText = `Hello ${username}`;
+}
+
+const savedUsername = localStorage.getItem(USERNAME_KEY);
+
+if (savedUsername === null) {
+    // show the form
+    loginForm.classList.remove(HIDDEN_CLASSNAME);
+    loginForm.addEventListener("submit", onLoginSubmit);
+} else {
+    // show the greeting
+    showGreeting(savedUsername);
+}
